f6eca67af3cde8df0f6c08828a1faf2c6424ec30
siddarood <siddusid081@gmail.com>
2023-12-02 06:41:58 +0530

    deploying flightReservationApp

---
+++ flight_reservation_app_6/src/main/java/com/psa/flightapp/FlightReservationApp6Application.java
@@ -0,0 +1,14 @@
+package com.psa.flightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightReservationApp6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightReservationApp6Application.class, args);
+		System.out.println("hii i am siddarood and i am a Java Developer");
+	}
+
+}
